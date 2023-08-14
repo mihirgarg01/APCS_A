a37f1b2bbe3c275a09350830d089f2e6a86e8530
mihirgarg01 <hepmihir@gmail.com>
2023-08-13 19:01:53 -0700

    Unit 1 lesson 1

---
+++ helloWorld.java
@@ -0,0 +1,18 @@
+package APCS_A;
+
+/*
+* Last Name: Garg
+* First Name: Mihir
+* Student ID: 12104703
+* Period: 4
+*
+* Hello World In Java
+* 
+*/
+public class helloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
