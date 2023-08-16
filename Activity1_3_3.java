3cd4fbdae2a35f82fc8b059140b3bdbbc5eb2d1f
mihirgarg01 <hepmihir@gmail.com>
2023-08-15 17:33:01 -0700

    Finished Lesson 3

---
+++ Activity1_3_3.java
@@ -0,0 +1,27 @@
+package APCS_A;
+/*
+* Last Name: Garg
+* First Name: Mihir
+* Student ID: 12104703
+* Period: 4
+*
+* Do the following:
+* 1. Create two String variables called firstName and LastName respectively.
+* 2. Assign the String "Bob" to firstName and "Jones" to lastName.
+* 3. Print firstName and lastName, one per line. Each should use a different print statement
+* 
+*/
+//Add the complete program below
+
+public class Activity1_3_3 {
+    public static void main(String[] args) {
+        String firstName = "";
+        String lastName = "";
+
+        firstName = "Bob";
+        lastName = "Jones";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+    }
+}
